a235d7a9ab7aa75196b8a0fef5162f069e912399
getssh <gtamrat33@gmail.com>
2024-03-04 13:58:34 +0300

    Add backend directory and entery point server.js

---
+++ backend/server.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const dotenv = require('dotenv').config()
+const port = 5000
+
+const app = express()
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`)
+})
