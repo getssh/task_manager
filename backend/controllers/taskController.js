b03abb699835d380f8b400d70d02fb441837551d
getssh <gtamrat33@gmail.com>
2024-03-04 22:45:49 +0300

    Add getTask and createTask function and connect with db

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -1,9 +1,25 @@
 const asyncHandler = require('express-async-handler')
+const Task = require('../models/taskModel')
 
 const getTasks = asyncHandler(async(req, res) => {
-  res.status(200).json({message: 'Test get tasks'})
+  const tasks = await Task.find()
+
+  res.status(200).json(tasks)
+})
+
+const createTask = asyncHandler(async(req, res) => {
+  const {taskItem, priority, taskCompleted} = req.body
+
+  if (!taskItem || !priority) {
+    res.status(400)
+    throw new Error('Please fill all required fields')
+  } else {
+    const task = await Task.create({taskItem, priority, taskCompleted})
+    res.status(200).json(task)
+  }
 })
 
 module.exports = {
-  getTasks
+  getTasks,
+  createTask,
 }
