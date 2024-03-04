1fc392ea6265061cf22307b18e0d7e694f5b47c7
getssh <gtamrat33@gmail.com>
2024-03-04 14:35:53 +0300

    Add getTask and imported routes

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,11 @@ const mongoDB = require('./config/db')
 const app = express()
 mongoDB()
 
+app.use('/api/tasks', require('./routes/taskRoute'))
+
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
 app.listen(port, () => {
   console.log(`Server running on port ${port}`)
 })
