78db53f9325c185fc36d645f9abdfd4d04f7ca76
getssh <gtamrat33@gmail.com>
2024-03-05 20:27:08 +0300

    Update task routes to protect them

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -1,8 +1,9 @@
 const asyncHandler = require('express-async-handler')
 const Task = require('../models/taskModel')
+const User = require('../models/userModel')
 
 const getTasks = asyncHandler(async(req, res) => {
-  const tasks = await Task.find()
+  const tasks = await Task.find({user: req.user})
 
   res.status(200).json(tasks)
 })
@@ -14,7 +15,7 @@ const createTask = asyncHandler(async(req, res) => {
     res.status(400)
     throw new Error('Please fill all required fields')
   } else {
-    const task = await Task.create({taskItem, priority, taskCompleted})
+    const task = await Task.create({user:req.user, taskItem, priority, taskCompleted})
     res.status(200).json(task)
   }
 })
@@ -25,6 +26,11 @@ const updateTask = asyncHandler(async(req, res) => {
   if (!task) {
     res.status(404)
     throw new Error('Task not found')
+  }
+
+  if (task.user.toString() !== req.user.id) {
+    res.status(401)
+    throw new Error('Not authoraized')
   }
 
   const updatedTask = await Task.findByIdAndUpdate(req.params.id, req.body, {new: true})
--- backend/routes/taskRoute.js
+++ backend/routes/taskRoute.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const router = express.Router()
 const {getTasks, createTask, updateTask, deleteTask} = require('../controllers/taskController')
+const {protect} = require('../middleWare/authMiddleware')
 
-router.route('/').get(getTasks).post(createTask)
-router.route('/:id').put(updateTask).delete(deleteTask)
+router.route('/').get(protect, getTasks).post(protect, createTask)
+router.route('/:id').put(protect, updateTask).delete(protect, deleteTask)
 
 module.exports = router
