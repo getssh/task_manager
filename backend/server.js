6a88cd05700298f7dca8377e1a48e05274f70fe3
getssh <gtamrat33@gmail.com>
2024-03-05 11:15:26 +0300

    Add user router

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
 app.use('/api/tasks', require('./routes/taskRoute'))
+app.use('/api/users', require('./routes/userRoute'))
 app.use(errorHandler)
 
 app.listen(port, () => {
