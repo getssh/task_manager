4c4cc6f511bfd828cbc013a1c7c03b49aef5536f
getssh <gtamrat33@gmail.com>
2024-03-05 12:56:21 +0300

    Fix authmiddleware protect function

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,17 +1,15 @@
 const asyncHandler = require('express-async-handler')
 const User = require('../models/userModel')
 const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
 
 const getUser = asyncHandler(async(req, res) => {
-  const user = await User.findById(req.user.id)
-
-  if (!user) {
+ if (!req.user) {
     res.status(401)
     throw new Error('User not found')
   }
 
-  res.status(200)
-  res.json(user)
+  res.status(200).json(req.user)
 })
 
 const registerUser = asyncHandler(async(req, res) => {
@@ -29,15 +27,24 @@ const registerUser = asyncHandler(async(req, res) => {
     throw new Error('User already exists')
   }
 
-  const user = await User.create({name, email, password})
-  res.status(200)
-  res.json({
-    id: user._id,
-    name,
-    email,
-    token: generateToken(user._id),
-  })
-})
+  const salt = await bcrypt.genSalt(10)
+  const hashedPassword = await bcrypt.hash(password, salt)
+
+  const user = await User.create({name, email, password: hashedPassword})
+
+  if (user) {
+    res.status(201)
+    res.json({
+      _id: user.id,
+      name: user.name,
+      email: user.email,
+      token: generateToken(user._id),
+    })
+  } else {
+    res.status(400)
+    throw new Erorr('Invalid user data')
+  }
+})  
 
 const generateToken = (id) => {
   return jwt.sign({id}, process.env.JWT_SECRET, {
--- backend/middleWare/authMiddleware.js
+++ backend/middleWare/authMiddleware.js
@@ -6,10 +6,10 @@ const protect = asyncHandler(async(req, res, next) => {
   let token
   if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
     try {
-      token = req.headers.Authorization.split(' ')[1]
+      token = req.headers.authorization.split(' ')[1]
 
       const decoded = jwt.verify(token, process.env.JWT_SECRET)
-      res.user = await User.findById(decoded.id).select('-password')
+      req.user = await User.findById(decoded.id).select('-password')
       next()
     } catch (error) {
       console.log(error)
