b57da06e0a3baac8ac8f4def7ffde0ee48daf595
getssh <gtamrat33@gmail.com>
2024-03-05 12:18:27 +0300

    Add register user and get user function

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -15,11 +15,32 @@ const getUser = asyncHandler(async(req, res) => {
 })
 
 const registerUser = asyncHandler(async(req, res) => {
+  const {name, email, password} = req.body
 
+  if (!name || !email || !password) {
+    res.status(400)
+    throw new Error('Please fill all required feilds')
+  }
+
+  const userExists = await User.findOne({email})
+
+  if (userExists) {
+    res.status(400)
+    throw new Error('User already exists')
+  }
+
+  const user = await User.create({name, email, password})
+  res.status(200)
+  res.json({
+    id: user._id,
+    name,
+    email,
+    token: generateToken(user._id),
+  })
 })
 
 const generateToken = (id) => {
-  return jwt.sign({id}, process.env.JWT_TOKEN, {
+  return jwt.sign({id}, process.env.JWT_SECRET, {
     expiresIn: '7d'
   })
 }
