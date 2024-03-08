8939800ecedd824f64bb5e6cd058c022665c891b
getssh <gtamrat33@gmail.com>
2024-03-08 17:25:11 +0300

    Add function for register and signin and import it to store

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import TaskReducer from '../features/tasks/taskSlice'
+import UserReducer from '../features/users/userSlice'
 
 export const store = configureStore({
   reducer: {
     tasks: TaskReducer,
+    user: UserReducer,
   },
 });
--- frontend/src/features/users/userSlice.js
+++ frontend/src/features/users/userSlice.js
@@ -1,14 +1,33 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import userHelper from './userHelper'
 
 const user = JSON.parse(localStorage.getItem('user'))
 
 const initialState = {
-  user: user ? user : [],
+  user: user ? user : null,
   isLoading: false,
   isError: false,
   isSuccess: false,
   message: "",
 }
+
+export const registerUser = createAsyncThunk('/register', async(userData, thunkAPI) => {
+  try {
+    return await userHelper.registerUser(userData)
+  } catch (error) {
+    console.log(error)
+    thunkAPI.rejectWithValue(error)
+  }
+})
+
+export const signinUser = createAsyncThunk('/signin', async(userData, thunkAPI) => {
+  try {
+    return await userHelper.signinUser(userData)
+  } catch (error) {
+    console.log(error)
+    thunkAPI.rejectWithValue(error)
+  }
+})
 
 const userSlice = createSlice({
   name: "user",
@@ -22,7 +41,35 @@ const userSlice = createSlice({
     }
   },
   extraReducers: (builder) => {
-
+    builder
+      .addCase(registerUser.pending, (state) => {
+        state.isLoading = true
+      })
+      .addCase(registerUser.fulfilled, (state, action)=> {
+        state.isLoading = false
+        state.isSuccess = true
+        state.user = action.payload
+      })
+      .addCase(registerUser.rejected, (state, action) => {
+        state.isLoading = false
+        state.isError = true
+        state.user = null
+        state.message = action.payload
+      })
+      .addCase(signinUser.pending, (state) => {
+        state.isLoading = true
+      })
+      .addCase(signinUser.fulfilled, (state, action)=> {
+        state.isLoading = false
+        state.isSuccess = true
+        state.user = action.payload
+      })
+      .addCase(signinUser.rejected, (state, action) => {
+        state.isLoading = false
+        state.isError = true
+        state.user = null
+        state.message = action.payload
+      })
   }
 })
 
