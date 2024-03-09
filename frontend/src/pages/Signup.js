c5cc7ba7257a855284f79df646f04bb6f1ad99ba
getssh <gtamrat33@gmail.com>
2024-03-09 13:05:13 +0300

    Add user registration page

--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -1,24 +1,83 @@
-import { AccountCircle } from '@mui/icons-material'
-import { Box, FormControl, Input, InputAdornment, InputLabel } from '@mui/material'
-import React from 'react'
+import styled from '@emotion/styled'
+import { AccountCircle, EmailRounded, Key, Visibility, VisibilityOff } from '@mui/icons-material'
+import { Box, Button, FormControl, IconButton, Input, InputAdornment, InputLabel, Typography } from '@mui/material'
+import React, { useState } from 'react'
 
+const FormBox = styled(Box)({
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "flex-end",
+  padding: "10px",
+  marginTop: "5%",
+})
 const Signup = () => {
+  const [user, setUser] = useState({
+    name: "",
+    email: "",
+    password: "",
+    showPassword: false,
+  })
   return (
-    <Box>
-      <FormControl variant="standard">
-        <InputLabel htmlFor="input-with-icon-adornment">
-          With a start adornment
-        </InputLabel>
-        <Input
-          id="input-with-icon-adornment"
-          startAdornment={
-            <InputAdornment position="start">
-              <AccountCircle />
-            </InputAdornment>
-          }
-        />
-      </FormControl>
-    </Box>
+    <>
+      <Typography variant='h4' textAlign="center" mt={3}>New User Registration</Typography>
+      <FormBox>
+        <Box sx={{bgcolor: "lightcyan", display:"flex", gap:5,
+          flexDirection: "column", width: {xs:"90%", sm:"50%", md:"50%", lg:"30%"},
+          borderRadius: 5,
+        }} p={5}>
+          <FormControl variant="standard">
+            <Input
+              id="input-with-icon-adornment"
+              type='text'
+              placeholder='Name'
+              startAdornment={
+                <InputAdornment position="start">
+                  <AccountCircle />
+                </InputAdornment>
+              }
+            />
+          </FormControl>
+          <FormControl variant="standard">
+            <Input
+              id="input-with-icon-adornment"
+              type='email'
+              placeholder='Email Address'
+              startAdornment={
+                <InputAdornment position="start">
+                  <EmailRounded />
+                </InputAdornment>
+              }
+            />
+          </FormControl>
+          <FormControl variant="standard">
+            <Input
+              id="input-with-icon-adornment"
+              type={user.showPassword ? 'text' : 'password'}
+              placeholder='Email Address'
+              startAdornment={
+                <InputAdornment position="start">
+                  <Key />
+                </InputAdornment>
+              }
+              endAdornment={
+                <InputAdornment position="start">
+                  <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={()=>setUser({...user, showPassword: !user.showPassword})}
+                  edge="end"
+                  >
+                  {user.showPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+                </InputAdornment>
+              }
+            />
+          </FormControl>
+          <FormControl>
+            <Button variant="contained">Sign Up</Button>
+          </FormControl>
+        </Box>
+      </FormBox>
+    </>
   )
 }
 
