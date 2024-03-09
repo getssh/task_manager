3e0e8908837ce536c44e55ef28ce07c1d55e3d85
getssh <gtamrat33@gmail.com>
2024-03-09 18:24:47 +0300

    Add signup functionality with store

--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -1,7 +1,11 @@
 import styled from '@emotion/styled'
 import { AccountCircle, EmailRounded, Key, Visibility, VisibilityOff } from '@mui/icons-material'
-import { Box, Button, FormControl, IconButton, Input, InputAdornment, InputLabel, Typography } from '@mui/material'
-import React, { useState } from 'react'
+import { Box, Button, FormControl, IconButton, Input, InputAdornment, Typography } from '@mui/material'
+import { useState, useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import {registerUser, reset} from '../features/users/userSlice'
+import { toast } from 'react-toastify'
 
 const FormBox = styled(Box)({
   display: "flex",
@@ -11,25 +15,72 @@ const FormBox = styled(Box)({
   marginTop: "5%",
 })
 const Signup = () => {
-  const [user, setUser] = useState({
+  const [userData, setUserData] = useState({
     name: "",
     email: "",
     password: "",
     confirmPassword: "",
     showPassword: false,
   })
+
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+
+  const {name, email, password, confirmPassword} = userData
+  const {user, isLoading, isError, isSuccess, message} = useSelector((state)=> state.user)
+
+  useEffect(()=> {
+    if (isError) {
+      toast.error(message)
+    }
+
+    if (isSuccess || user) {
+      navigate('/')
+    }
+
+    dispatch(reset())
+  }, [isError, isSuccess, message, user, dispatch, navigate])
+
+  const handleChange = (e) => {
+    const {name, value} = e.target
+
+    setUserData({
+      ...userData,
+      [name]: value,
+    })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    if (password !== confirmPassword) {
+      toast.error("Password do not match")
+    } else {
+      const userInfo = {name, email, password}
+
+      dispatch(registerUser(userInfo))
+    }
+  }
+
+  if (isLoading) {
+    return <div>Loading</div>
+  }
+
   return (
     <>
       <Typography variant='h4' textAlign="center" mt={3}>New User Registration</Typography>
       <FormBox>
-        <Box sx={{bgcolor: "lightcyan", display:"flex", gap:5,
-          flexDirection: "column", width: {xs:"90%", sm:"50%", md:"50%", lg:"30%"},
+        <Box component="form" onSubmit={handleSubmit} sx={{bgcolor: "lightcyan", display:"flex", gap:5,
+          flexDirection: "column", width: {xs:"90%", sm:"50%", md:"50%", lg:"40%"},
           borderRadius: 5,
         }} p={5}>
           <FormControl variant="standard">
             <Input
               type='text'
               placeholder='Name'
+              name="name"
+              value={name}
+              onChange={handleChange}
               startAdornment={
                 <InputAdornment position="start">
                   <AccountCircle />
@@ -41,6 +92,9 @@ const Signup = () => {
             <Input
               type='email'
               placeholder='Email Address'
+              name="email"
+              value={email}
+              onChange={handleChange}
               startAdornment={
                 <InputAdornment position="start">
                   <EmailRounded />
@@ -50,8 +104,11 @@ const Signup = () => {
           </FormControl>
           <FormControl variant="standard">
             <Input
-              type={user.showPassword ? 'text' : 'password'}
+              type={userData.showPassword ? 'text' : 'password'}
               placeholder='Password'
+              name="confirmPassword"
+              value={confirmPassword}
+              onChange={handleChange}
               startAdornment={
                 <InputAdornment position="start">
                   <Key />
@@ -61,10 +118,10 @@ const Signup = () => {
                 <InputAdornment position="start">
                   <IconButton
                   aria-label="toggle password visibility"
-                  onClick={()=>setUser({...user, showPassword: !user.showPassword})}
+                  onClick={()=>setUserData({...userData, showPassword: !userData.showPassword})}
                   edge="end"
                   >
-                  {user.showPassword ? <VisibilityOff /> : <Visibility />}
+                  {userData.showPassword ? <VisibilityOff /> : <Visibility />}
                 </IconButton>
                 </InputAdornment>
               }
@@ -72,8 +129,11 @@ const Signup = () => {
           </FormControl>
           <FormControl variant="standard">
             <Input
-              type={user.showPassword ? 'text' : 'password'}
+              type={userData.showPassword ? 'text' : 'password'}
               placeholder='Password'
+              name="password"
+              value={password}
+              onChange={handleChange}
               startAdornment={
                 <InputAdornment position="start">
                   <Key />
@@ -83,17 +143,17 @@ const Signup = () => {
                 <InputAdornment position="start">
                   <IconButton
                   aria-label="toggle password visibility"
-                  onClick={()=>setUser({...user, showPassword: !user.showPassword})}
+                  onClick={()=>setUserData({...userData, showPassword: !userData.showPassword})}
                   edge="end"
                   >
-                  {user.showPassword ? <VisibilityOff /> : <Visibility />}
+                  {userData.showPassword ? <VisibilityOff /> : <Visibility />}
                 </IconButton>
                 </InputAdornment>
               }
             />
           </FormControl>
           <FormControl>
-            <Button variant="contained">Sign Up</Button>
+            <Button variant="contained" type='submit'>Sign Up</Button>
           </FormControl>
         </Box>
       </FormBox>
