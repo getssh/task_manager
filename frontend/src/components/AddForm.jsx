6fa474be939aa2b3fcc2929b37cb7b1b0c351052
getssh <gtamrat33@gmail.com>
2024-03-11 22:56:37 +0300

    Update addform to take task data

--- frontend/src/components/AddForm.jsx
+++ frontend/src/components/AddForm.jsx
@@ -1,9 +1,30 @@
-import { EmailRounded, Key, Visibility } from '@mui/icons-material'
-import { Box, Button, FormControl, IconButton, Input, InputAdornment, Modal, Typography } from '@mui/material'
+import { EmailRounded, Key, TaskAlt, TaskSharp, Visibility } from '@mui/icons-material'
+import { Box, Button, FormControl, IconButton, Input, InputAdornment, MenuItem, Modal, Select, Typography } from '@mui/material'
 import React, { useState } from 'react'
 
 const AddForm = () => {
   const [open, setOpen] = useState(false)
+
+  const [formData, setFormData] = useState({
+    taskItem: "",
+    priority: "low",
+  })
+
+  const {taskItem, priority} = formData
+
+  const handleChange = (e) => {
+    const {name, value} = e.target
+
+    setFormData({
+      ...formData,
+      [name]: value,
+    })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(formData)
+  }
 
   return (
     <>
@@ -15,46 +36,40 @@ const AddForm = () => {
         aria-describedby="modal-modal-description"
         sx={{display: 'flex', justifyContent:"center", alignItems:"center"}}
       >
-        <Box component="form" sx={{bgcolor: "lightcyan", display:"flex", gap:5,
+        <Box
+          onSubmit={handleSubmit}
+          component="form" sx={{bgcolor: "lightcyan", display:"flex", gap:5,
           flexDirection: "column", width: {xs:"90%", sm:"50%", md:"50%", lg:"30%"},
           borderRadius: 5,
         }} p={5}>
           <FormControl variant="standard">
             <Input
-              type='email'
-              placeholder='Email Address'
-              name='email'
+              type='text'
+              placeholder='Task Info'
+              name='taskItem'
+              value={taskItem}
+              onChange={handleChange}
               startAdornment={
                 <InputAdornment position="start">
-                  <EmailRounded />
+                  <TaskSharp />
                 </InputAdornment>
               }
             />
           </FormControl>
-          <FormControl variant="standard">
-            <Input
-              type='password'
-              placeholder='Password'
-              name='password'
-              startAdornment={
-                <InputAdornment position="start">
-                  <Key />
-                </InputAdornment>
-              }
-              endAdornment={
-                <InputAdornment position="start">
-                  <IconButton
-                  aria-label="toggle password visibility"
-                  edge="end"
-                  >
-                  <Visibility />
-                </IconButton>
-                </InputAdornment>
-              }
-            />
+          <FormControl fullWidth>
+            <Select
+              name="priority"
+              label="Priority"
+              placeholder='Priority'
+              onChange={handleChange}
+            >
+              <MenuItem component="option" value="low">Low</MenuItem>
+              <MenuItem component="option" value="high">High</MenuItem>
+              <MenuItem component="option" value="urgent">Urgent</MenuItem>
+            </Select>
           </FormControl>
           <FormControl>
-            <Button variant="contained" type='submit'>LogIn</Button>
+            <Button variant="contained" type='submit'>Add Task</Button>
           </FormControl>
         </Box>
       </Modal>
