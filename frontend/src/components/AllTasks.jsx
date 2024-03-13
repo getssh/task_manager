1bbdcc97f0044045e52a074309ea394aafd4f863
getssh <gtamrat33@gmail.com>
2024-03-13 13:25:02 +0300

    Add card to render task lists

--- frontend/src/components/AllTasks.jsx
+++ frontend/src/components/AllTasks.jsx
@@ -1,10 +1,11 @@
-import { Box } from '@mui/material'
+import { Box, Button, Card, CardActions, CardContent, IconButton, Typography } from '@mui/material'
 import { useEffect } from 'react'
 import {useSelector, useDispatch} from 'react-redux'
 import {useNavigate} from 'react-router-dom'
 import {getTasks, reset} from '../features/tasks/taskSlice'
 import {toast} from 'react-toastify'
 import AddForm from './AddForm'
+import { Favorite, Share } from '@mui/icons-material'
 
 const AllTasks = () => {
   const dispatch = useDispatch()
@@ -40,7 +41,29 @@ const AllTasks = () => {
             {
               tasks.map((task)=> {
                 return <div key={task.id}>
-                  <p>{task.taskItem}</p>
+                      <Box sx={{ minWidth: 275 }}>
+                      <Card variant="outlined">
+                      <CardContent>
+                        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                          Task Details
+                        </Typography>
+                        <Typography variant="h5" component="div">
+                          {task.taskItem}
+                        </Typography>
+                        <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                          Not completed
+                        </Typography>
+                        </CardContent>
+                        <CardActions>
+                          <IconButton aria-label="add to favorites">
+                            <Favorite />
+                          </IconButton>
+                          <IconButton aria-label="share">
+                            <Share />
+                          </IconButton>
+                        </CardActions>
+                      </Card>
+                    </Box>
                 </div>
               })
             }
