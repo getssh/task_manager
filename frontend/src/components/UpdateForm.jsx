724010c836a5511e831272a22f7a23c338ad2428
getssh <gtamrat33@gmail.com>
2024-03-14 12:48:17 +0300

    Add updateform for updating tasks

---
+++ frontend/src/components/UpdateForm.jsx
@@ -0,0 +1,96 @@
+import { Edit, TaskSharp } from '@mui/icons-material'
+import { Box, Button, FormControl, IconButton, Input, InputAdornment, MenuItem, Modal, Select, Typography } from '@mui/material'
+import { useEffect, useState } from 'react'
+import {useSelector, useDispatch} from 'react-redux'
+import {addTask, getTasks, updatedTask} from '../features/tasks/taskSlice'
+import {useNavigate} from 'react-router-dom'
+
+const UpdateForm = ({taskId}) => {
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+
+  const [open, setOpen] = useState(false)
+
+  const [formData, setFormData] = useState({
+    taskItem: "",
+    priority: "low",
+  })
+
+  const {taskItem, priority} = formData
+
+  const handleChange = (e) => {
+    const {name, value} = e.target
+
+    setFormData({
+      ...formData,
+      [name]: value,
+    })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    const taskData = {taskItem, priority}
+
+    dispatch(updatedTask({taskId, taskData}))
+    setFormData({
+        taskItem: "",
+        priority: "low",
+    })
+    setOpen(false)
+  }
+
+  return (
+    <>
+      <IconButton aria-label="edit">
+        <Edit onClick={()=>setOpen(true)}/>
+      </IconButton>
+      <Modal
+        open={open}
+        onClose={()=>setOpen(false)}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+        sx={{display: 'flex', justifyContent:"center", alignItems:"center"}}
+      >
+        <Box
+          onSubmit={handleSubmit}
+          component="form" sx={{bgcolor: "lightcyan", display:"flex", gap:5,
+          flexDirection: "column", width: {xs:"90%", sm:"50%", md:"50%", lg:"30%"},
+          borderRadius: 5,
+        }} p={5}>
+          <FormControl variant="standard">
+            <Input
+              type='text'
+              placeholder='Task Info'
+              name='taskItem'
+              value={taskItem}
+              onChange={handleChange}
+              startAdornment={
+                <InputAdornment position="start">
+                  <TaskSharp />
+                </InputAdornment>
+              }
+            />
+          </FormControl>
+          <FormControl fullWidth>
+            <Select
+              name="priority"
+              label="Priority"
+              placeholder='Priority'
+              onChange={handleChange}
+            >
+              <MenuItem component="option" value="low">Low</MenuItem>
+              <MenuItem component="option" value="high">High</MenuItem>
+              <MenuItem component="option" value="urgent">Urgent</MenuItem>
+            </Select>
+          </FormControl>
+          <FormControl>
+            <Button variant="contained" type='submit'>Add Task</Button>
+          </FormControl>
+        </Box>
+      </Modal>
+    </>
+  )
+}
+
+export default UpdateForm
