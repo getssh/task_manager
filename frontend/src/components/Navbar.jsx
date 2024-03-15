10b667732892410711b7157e849a0e5541c7a3b5
getssh <gtamrat33@gmail.com>
2024-03-15 23:10:30 +0300

    Update navbar to be sticky

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -18,64 +18,62 @@ const Navbar = () => {
   }
 
   return (
-    <Box>
-      <AppBar position="static" sx={{bgcolor: "darkcyan"}}>
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="open drawer"
-            sx={{ mr: 2 }}
-          >
-            <TaskAlt />
-          </IconButton>
-          <Typography
-            variant="h6"
-            noWrap
-            component="div"
-            sx={{ display: { xs: 'none', sm: 'block' } }}
-          >
-            Task Manager
-          </Typography>
-          <Box sx={{ flexGrow: 1 }} />
+    <AppBar position="sticky" sx={{bgcolor: "darkcyan"}}>
+      <Toolbar>
+        <IconButton
+          size="large"
+          edge="start"
+          color="inherit"
+          aria-label="open drawer"
+          sx={{ mr: 2 }}
+        >
+          <TaskAlt />
+        </IconButton>
+        <Typography
+          variant="h6"
+          noWrap
+          component="div"
+          sx={{ display: { xs: 'none', sm: 'block' } }}
+        >
+          Task Manager
+        </Typography>
+        <Box sx={{ flexGrow: 1 }} />
 
-         
-          {
-            user ? 
-            <Box sx={{ display: { xs: 'none', sm: 'flex' }, alignItems: "center" }}>
-              <Button component={Link} to="/" color="inherit" exact>
-                Tasks
-              </Button>
-              <Button component={Link} to="/liked" color="inherit">
-                Favoriate
-              </Button>
+        
+        {
+          user ? 
+          <Box sx={{ display: { xs: 'none', sm: 'flex' }, alignItems: "center" }}>
+            <Button component={Link} to="/" color="inherit" exact>
+              Tasks
+            </Button>
+            <Button component={Link} to="/liked" color="inherit">
+              Favoriate
+            </Button>
 
-              <Button color="inherit"
-                onClick={handleSignout}
-              >
-                Sign out
-              </Button>
-                <Avatar
-                  component={Link}
-                  to="/profile"
-                  src="/broken-image.jpg"
-                />
-              </Box>
-             :
-             <Box sx={{ display: { xs: 'none', sm: 'flex' }, alignItems: "center" }}> 
-              <Button component={Link} to="/signin" color="inherit">
-                Signin
-              </Button>
-              <Button component={Link} to="/signup" color="inherit">
-                Signup
-              </Button>
+            <Button color="inherit"
+              onClick={handleSignout}
+            >
+              Sign out
+            </Button>
+              <Avatar
+                component={Link}
+                to="/profile"
+                src="/broken-image.jpg"
+              />
             </Box>
-          }
-          <Menu sx={{display: {xs: "block", sm:"none"}}}/>
-        </Toolbar>
-      </AppBar>
-    </Box>
+            :
+            <Box sx={{ display: { xs: 'none', sm: 'flex' }, alignItems: "center" }}> 
+            <Button component={Link} to="/signin" color="inherit">
+              Signin
+            </Button>
+            <Button component={Link} to="/signup" color="inherit">
+              Signup
+            </Button>
+          </Box>
+        }
+        <Menu sx={{display: {xs: "block", sm:"none"}}}/>
+      </Toolbar>
+    </AppBar>
   )
 }
 
