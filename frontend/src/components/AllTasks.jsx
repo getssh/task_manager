ecaae08afa594ed4956ba35bb6f0ca3a45b8b53e
getssh <gtamrat33@gmail.com>
2024-03-15 17:01:32 +0300

    Add Task component to render by itself

--- frontend/src/components/AllTasks.jsx
+++ frontend/src/components/AllTasks.jsx
@@ -2,11 +2,11 @@ import { Box, Button, Card, CardActions, CardContent, Checkbox, IconButton, Typo
 import { useEffect } from 'react'
 import {useSelector, useDispatch} from 'react-redux'
 import {useNavigate} from 'react-router-dom'
-import {getTasks, removeTask, reset} from '../features/tasks/taskSlice'
+import {getTasks, removeTask, reset, updatedTask} from '../features/tasks/taskSlice'
 import {toast} from 'react-toastify'
-import { updatedTask } from '../features/tasks/taskSlice'
-import { Delete, Edit, Favorite, FavoriteBorder, Share } from '@mui/icons-material'
+import { Delete, Favorite, FavoriteBorder } from '@mui/icons-material'
 import UpdateForm from './UpdateForm'
+import Task from './Task'
 
 const AllTasks = () => {
   const dispatch = useDispatch()
@@ -43,41 +43,7 @@ const AllTasks = () => {
             tasks.map((task) => {
               return (
                 <div key={task._id}>
-                  <Box sx={{ minWidth: 275 }}>
-                    <Card variant="outlined" sx={{boxShadow: '1px 1px 2px gray',
-                      backgroundColor: task.taskCompleted ? 'lightgrey' : task.priority === 'high' && !task.taskCompleted ? 'lightyellow' :
-                      task.priority === 'urgent' && !task.taskCompleted ? 'lightpink' : 'white'
-                    }}
-                    >
-                    <CardContent>
-                    <Checkbox sx={{ml: -2}}
-                      checked={task.taskCompleted ? 'checked' : ''}
-                      onClick={() => dispatch(updatedTask({ taskId: task._id, taskData: { taskCompleted: !task.taskCompleted } }))}
-                    /> Mark as complete
-                      <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-                        Task Details
-                      </Typography>
-                      <Typography variant="p" component="div">
-                        {task.taskItem}
-                      </Typography>
-                      <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                        {task.taskCompleted ? 'Completed' : 'Not completed'}
-                      </Typography>
-                      </CardContent>
-                      <CardActions>
-                        <Checkbox icon={task.favorite ? <Favorite sx={{color: 'red'}}/> : <FavoriteBorder />} 
-                        checkedIcon={task.favorite ? <Favorite sx={{color: 'red'}}/> : <FavoriteBorder />}
-                          onClick={() => dispatch(updatedTask({ taskId: task._id, taskData: { favorite: !task.favorite } }))}
-                        />
-                        <UpdateForm taskToUpdate={task}/>
-                        <IconButton aria-label="delete"
-                          onClick={()=>{dispatch(removeTask(task._id))}}
-                        >
-                          <Delete />
-                        </IconButton>
-                      </CardActions>
-                    </Card>
-                  </Box>
+                  <Task task={task}/>
                 </div>
               );
             })
--- frontend/src/components/Task.jsx
+++ frontend/src/components/Task.jsx
@@ -1,8 +1,48 @@
-import React from 'react'
+import { Delete, Favorite, FavoriteBorder } from '@mui/icons-material'
+import { Box, Card, CardActions, CardContent, Checkbox, IconButton, Typography } from '@mui/material'
+import { removeTask, updatedTask } from '../features/tasks/taskSlice'
+import UpdateForm from './UpdateForm'
+import {useDispatch} from 'react-redux'
 
-const Task = () => {
+const Task = ({task}) => {
+  const dispatch = useDispatch()
+
   return (
-    <div>Task</div>
+    <Box sx={{ minWidth: 275 }}>
+    <Card variant="outlined" sx={{boxShadow: '1px 1px 2px gray',
+      backgroundColor: task.taskCompleted ? 'lightgrey' : task.priority === 'high' && !task.taskCompleted ? 'lightyellow' :
+      task.priority === 'urgent' && !task.taskCompleted ? 'lightpink' : 'white'
+    }}
+    >
+    <CardContent>
+    <Checkbox sx={{ml: -2}}
+      checked={task.taskCompleted ? 'checked' : ''}
+      onClick={() => dispatch(updatedTask({ taskId: task._id, taskData: { taskCompleted: !task.taskCompleted } }))}
+    /> Mark as complete
+      <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+        Task Details
+      </Typography>
+      <Typography variant="p" component="div">
+        {task.taskItem}
+      </Typography>
+      <Typography sx={{ mb: 1.5 }} color="text.secondary">
+        {task.taskCompleted ? 'Completed' : 'Not completed'}
+      </Typography>
+      </CardContent>
+      <CardActions>
+        <Checkbox icon={task.favorite ? <Favorite sx={{color: 'red'}}/> : <FavoriteBorder />} 
+        checkedIcon={task.favorite ? <Favorite sx={{color: 'red'}}/> : <FavoriteBorder />}
+          onClick={() => dispatch(updatedTask({ taskId: task._id, taskData: { favorite: !task.favorite } }))}
+        />
+        <UpdateForm taskToUpdate={task}/>
+        <IconButton aria-label="delete"
+          onClick={()=>{dispatch(removeTask(task._id))}}
+        >
+          <Delete />
+        </IconButton>
+      </CardActions>
+    </Card>
+  </Box>
   )
 }
 
