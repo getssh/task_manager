6a7d7d3c5e8fa4c5b478c60774dac9dac0262654
getssh <gtamrat33@gmail.com>
2024-03-16 13:07:11 +0300

    Add showtask component to show single task with modal

---
+++ frontend/src/components/ShowTask.js
@@ -0,0 +1,32 @@
+import { OpenInNew } from '@mui/icons-material'
+import { Box, IconButton, Modal, Tooltip } from '@mui/material'
+import React, { useState } from 'react'
+import Task from './Task'
+
+const ShowTask = ({taskItem}) => {
+  const [open, setOpen] = useState(false)
+  return (
+    <div>
+      <Tooltip
+        onClick={(e)=>setOpen(true)}
+        title="showtask">
+        <IconButton aria-label="delete">
+          <OpenInNew />
+        </IconButton>
+      </Tooltip>
+      <Modal
+        open={open}
+        onClose={()=>setOpen(false)}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+        sx={{display: 'flex', justifyContent:"center", alignItems:"center"}}
+      >
+      <Box>
+        <Task task={taskItem}/>
+      </Box>
+      </Modal>
+    </div>
+  )
+}
+
+export default ShowTask
