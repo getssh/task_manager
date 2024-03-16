05908e738058b865ed5f29299ffd027fbb80d726
getssh <gtamrat33@gmail.com>
2024-03-16 23:57:48 +0300

    Add profile page user info

--- frontend/src/pages/Profile.js
+++ frontend/src/pages/Profile.js
@@ -1,24 +1,34 @@
 import {useSelector} from 'react-redux'
-import { Typography } from '@mui/material'
+import { Box, Typography } from '@mui/material'
 
 const Profile = () => {
 
   const {user, isLoading} = useSelector((state)=>state.user)
+  const {tasks} = useSelector((state) => state.tasks)
 
+  console.log(user)
   if (isLoading) {
     <div>Loading...</div>
   }
   return (
     <div>
       {
-        user ? <div>
+        user ? <Box sx={{display: 'flex', flexDirection: 'column', 
+          height: '80vh',
+        justifyContent: 'center', alignItems: 'center', gap: 3}}>
           <Typography variant='h4'>
-            {user.name}
+            Name: {user.name}
           </Typography>
           <Typography variant='h4'>
-            {user.email}
+            Email: {user.email}
           </Typography>
-        </div> : 
+          <Typography variant='h4'>
+            Total Tasks: {tasks.length}
+          </Typography>
+          <Typography variant='h4'>
+            Account Created: {new Date(user.createdAt).toLocaleString('en-us')}
+          </Typography>
+        </Box> : 
         <div>No user found</div>
       }
     </div>
