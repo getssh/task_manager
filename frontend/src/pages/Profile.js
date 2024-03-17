913b95a0c3f3f1e70d53efdd91021b502130b88b
getssh <gtamrat33@gmail.com>
2024-03-17 14:48:17 +0300

    Update profile page to use Card and add delete account btn

--- frontend/src/pages/Profile.js
+++ frontend/src/pages/Profile.js
@@ -1,5 +1,5 @@
 import {useSelector} from 'react-redux'
-import { Box, Typography } from '@mui/material'
+import { Box, Button, Card, CardActions, CardContent, Typography } from '@mui/material'
 
 const Profile = () => {
 
@@ -13,28 +13,36 @@ const Profile = () => {
   return (
     <div>
       {
-        user ? <Box sx={{display: 'flex', flexDirection: 'column', 
-          height: '80vh',
-        justifyContent: 'center', alignItems: 'center', gap: 3}}>
-          <Typography variant='h5'>
-            Name: {user.name}
-          </Typography>
-          <Typography variant='h5'>
-            Email: {user.email}
-          </Typography>
-          <Typography variant='h5'>
-            Total Tasks: {tasks.length}
-          </Typography>
-          <Typography variant='h5'>
-          Account Created: {
-              new Date(user.createdAt).toLocaleDateString('en-US', {
-                year: 'numeric',
-                month: '2-digit',
-                day: '2-digit'
-              })
-            }
-          </Typography>
-        </Box> : 
+        user ? <Box sx={{display: 'flex', 
+        height: '80vh', justifyContent: 'center', alignItems: 'center'}}>
+          <Card sx={{display: 'flex', flexDirection: 'column',
+          justifyContent: 'center', alignItems: 'center', gap: 2, padding: '3rem'}}>
+            <CardContent>
+              <Typography p={1} variant='h5'>
+                <b>Name:</b> {user.name}
+              </Typography>
+              <Typography p={1} variant='h5'>
+                <b>Email:</b> {user.email}
+              </Typography>
+              <Typography p={1} variant='h5'>
+                <b>Total Tasks:</b> {tasks.length}
+              </Typography>
+              <Typography p={1} variant='h5'>
+              <b>Account Created:</b> {
+                  new Date(user.createdAt).toLocaleDateString('en-US', {
+                    year: 'numeric',
+                    month: '2-digit',
+                    day: '2-digit'
+                  })
+                }
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button size="small" variant='contained' color='error'>Delete Account</Button>
+            </CardActions>
+          </Card> 
+        </Box>
+        : 
         <div>No user found</div>
       }
     </div>
