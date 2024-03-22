54db9ae654a56d7d0a88fd4c85f40a205b62175f
getssh <gtamrat33@gmail.com>
2024-03-22 12:15:34 +0300

    Update getTask and addTask extrareducers to put the date both on tasks and filter

--- frontend/src/features/tasks/taskSlice.js
+++ frontend/src/features/tasks/taskSlice.js
@@ -127,6 +127,7 @@ const taskSlice = createSlice({
         state.isLoading = false
         state.isSuccess = true
         state.tasks.push(action.payload)
+        state.filter.push(action.payload)
       })
       .addCase(addTask.rejected, (state, action) => {
         state.isLoading = false
@@ -140,6 +141,9 @@ const taskSlice = createSlice({
         state.isLoading = false
         state.isSuccess = true
         state.tasks = state.tasks.filter((task) => {
+          return task._id !== action.payload.id
+        })
+        state.filter = state.tasks.filter((task) => {
           return task._id !== action.payload.id
         })
       })
