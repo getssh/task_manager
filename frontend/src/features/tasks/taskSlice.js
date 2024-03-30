8baed351542357f3cdd7bc664149a05b03b0849a
getssh <gtamrat33@gmail.com>
2024-03-30 12:22:03 +0300

    Fix fltering by search to include upper and lowercase

--- frontend/src/features/tasks/taskSlice.js
+++ frontend/src/features/tasks/taskSlice.js
@@ -98,7 +98,7 @@ const taskSlice = createSlice({
             task.priority === action.payload.priorityFilter : task
 
           return filterComplete && filterFavorite && filterPriority &&
-          task.taskItem.toLowerCase().includes(action.payload.taskText)
+          task.taskItem.toLowerCase().includes(action.payload.taskText.toLowerCase())
         })
 
       state.tasks = filteredtask;
